5abb94994f88a72fd3b468d3cbb001ccaf274456
Anandpandey0 <75582022+Anandpandey0@users.noreply.github.com>
2024-05-06 18:45:47 +0530

    Made a demo database
    DemoDb has options values for the filter oprions

---
+++ src/api/DemoDb.js
@@ -0,0 +1,68 @@
+export const jobRoles = [
+  { title: "Backend", category: "ENGINEERING" },
+  { title: "Frontend", category: "ENGINEERING" },
+  { title: "Fullstack", category: "ENGINEERING" },
+  { title: "IOS", category: "ENGINEERING" },
+  { title: "Flutter", category: "ENGINEERING" },
+  { title: "React Native", category: "ENGINEERING" },
+  { title: "Android", category: "ENGINEERING" },
+  { title: "Tech Lead", category: "ENGINEERING" },
+  { title: "Dev-Ops", category: "ENGINEERING" },
+  { title: "Data Engineer", category: "ENGINEERING" },
+  { title: "Data Science", category: "ENGINEERING" },
+  { title: "Computer Vision", category: "ENGINEERING" },
+  { title: "Nlp", category: "ENGINEERING" },
+  { title: "Deep Learning", category: "ENGINEERING" },
+  { title: "Test / QA", category: "ENGINEERING" },
+  { title: "Web3", category: "ENGINEERING" },
+  { title: "Data Infrastructure", category: "ENGINEERING" },
+  { title: "Designer", category: "DESIGN" },
+  { title: "Design Manager", category: "DESIGN" },
+  { title: "Graphic Designer", category: "DESIGN" },
+  // Add more options as needed
+];
+
+export const NumberOfEmployees = [
+  {
+    title: "1-10",
+  },
+  {
+    title: "11-20",
+  },
+  {
+    title: "21-50",
+  },
+  {
+    title: "101-200",
+  },
+  {
+    title: "201-500",
+  },
+  {
+    title: "500+",
+  },
+];
+
+export const Experience = [
+  { title: "1" },
+  { title: "2" },
+  { title: "3" },
+  { title: "4" },
+  { title: "5" },
+  { title: "6" },
+  { title: "7" },
+  { title: "8" },
+  { title: "9" },
+  { title: "10" },
+];
+
+export const BasePay = [
+  { title: "0L" },
+  { title: "10L" },
+  { title: "20L" },
+  { title: "30L" },
+  { title: "40L" },
+  { title: "50L" },
+  { title: "60L" },
+  { title: "70L" },
+];
